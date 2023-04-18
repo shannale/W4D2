6abd2c4381e34b2227e78789cca823cbd78a21e1
Shanna Le <shannatle@gmail.com>
2023-04-18 14:50:38 -0700

    create classes

---
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/employee.rb
@@ -0,0 +1,17 @@
+class Employee 
+    attr_accessor :salary, :boss, :name, :title
+
+    def initialize(name,title,salary,boss=nil)
+        @name = name 
+        @title = title
+        @salary = salary
+        @boss = boss
+    end 
+
+
+    def bonus(multiplier)
+        bonus = total_salaries * multiplier
+    end 
+
+end 
+
---
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/manager.rb
@@ -0,0 +1,26 @@
+require_relative "employee.rb"
+
+
+class Manager < Employee
+    attr_reader :employees
+    
+    def initialize(name,title,salary,boss = nil)
+        super(name,title,salary,boss)
+        @employees = []
+    end 
+
+    def total_employees
+        @employees << self if self.boss.nil?
+    end
+
+
+    def bonus(multiplier)
+        total_salaries * multiplier
+    end 
+end
+
+# ned = Manager.new("ned", "founder", 1_000_000, nil)
+# darren = Manager.new("darren", "ta manager", 78_000, "ned")
+
+# shawna = Manager.new('shawna', 'ta', 12_000, "darren")
+# david = Manager.new('david', 'ta', 10_000, 'darren')
